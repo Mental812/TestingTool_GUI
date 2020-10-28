8a2f58584f9135eb5825c285bbb568939eb6857f
mental <mental_chen@aetina.com>
2020-10-28 07:08:31 +0000

    add read BSP file name

--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
 import sys 	
 from PyQt5.QtWidgets import QApplication , QMainWindow
 from utils.UI_Class import Ui_MainWindow
-
+from utils.Information_Class import Information_Class
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):    
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.Info_class = Information_Class()
         
     def change_Text(self):
-        self.lineEdit_BSP.setText("go...")
-        self.lineEdit_JetPack.setText("s...")
-        self.lineEdit_Camera.setText( "sada...")
+        BSP,JetPack,Camera = self.Info_class.get_Information()
+        self.lineEdit_BSP.setText(BSP)
+        self.lineEdit_JetPack.setText(JetPack)
+        self.lineEdit_Camera.setText(Camera)
         
 if __name__=="__main__":  
     app = QApplication(sys.argv)  
--- utils/Information_Class.py
+++ utils/Information_Class.py
@@ -5,11 +5,19 @@ class Information_Class():
     def __init__(self):
         self.__BSP_String = "Wait"
         self.__JetPack_String = "Wait"
-        self.__Camera = False
+        self.__Camera = "None"
 
     def get_Information(self):
+        self.__get_BSP_Name()
         return self.__BSP_String ,self.__JetPack_String ,self.__Camera
     
     def __get_BSP_Name(self):
-        BSP_file = open("","r")
+        BSP_file = open("/proc/device-tree/nvidia,dtsfilename","r")
         BSP_Text = BSP_file.read()
+        BSP_list = BSP_Text.split("_")
+        self.__BSP_String = BSP_list[0] + "_" + BSP_list[1] + "_" + BSP_list[2]
+        self.__JetPack_String = "4." + BSP_list[1]
+        if len(BSP_list) >= 7:
+            self.__Camera = BSP_list[5]
+        else: 
+            self.__Camera = "NO Camera"
