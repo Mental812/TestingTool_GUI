bec3b41018ece6015ba5083098e1a8b8d9b2af28
mental <q3512768@gmail.com>
2020-11-25 21:46:46 -0800

    add tableview for item

--- Main.py
+++ Main.py
@@ -1,22 +1,46 @@
 import sys 	
-from PyQt5.QtWidgets import QApplication , QMainWindow
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from utils.UI_Class import Ui_MainWindow
 from utils.Information_Class import Information_Class
+
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):    
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
         self.Info_class = Information_Class()
         
-    def change_Text(self):
-        BSP,JetPack,Camera = self.Info_class.get_Information()
+    def init_information(self):
+        BSP,JetPack,Camera,Support = self.Info_class.get_Information()
+
         self.lineEdit_BSP.setText(BSP)
         self.lineEdit_JetPack.setText(JetPack)
         self.lineEdit_Camera.setText(Camera)
-        
+
+        if Support == "Yes":
+            self.lineEdit_Support.setStyleSheet("background: green;")
+            TestingItem_Name = self.Info_class.get_TestingItem_list() #get Testing item list 
+            self.__set_TableView(TestingItem_Name)
+        elif Support == "No":
+            self.lineEdit_Support.setStyleSheet("background: red;")
+        self.lineEdit_Support.setText(Support)
+
+    def __set_TableView(self,TestingItem_Name):
+        self.tableView.setEditTriggers(QAbstractItemView.NoEditTriggers) #close edit in UI
+        model =  QStandardItemModel(len(TestingItem_Name), 2)
+        model.setHorizontalHeaderLabels(['Item', 'Status'])
+        for row in range(len(TestingItem_Name)):
+            #print(TestingItem_Name[row])
+            item = QStandardItem(str(TestingItem_Name[row]))
+            model.setItem(row, 0, item)
+            #model.setItem(row, 1, "Wait...")
+        self.tableView.setModel(model)
+
+    
 if __name__=="__main__":  
     app = QApplication(sys.argv)  
     myWin = MyMainWindow()  
     myWin.show()
-    myWin.change_Text()  
-    sys.exit(app.exec_())  
+    myWin.init_information()  
+    sys.exit(app.exec_()) 
--- utils/Information_Class.py
+++ utils/Information_Class.py
@@ -7,40 +7,77 @@ class Information_Class():
         self.__BSP_String = "Wait"
         self.__JetPack_String = "Wait"
         self.__Camera = "None"
+    
         self.__BSP_list = []
         self.__dir_data = './data/TestingItem.csv'
+
+        self.__TestingItem_list = []
+
         self.__get_BSP_Name()
+        self.__TestingItem_list = self.__get_TestingItem()
+
     def get_Information(self):
-        
-        return self.__BSP_String ,self.__JetPack_String ,self.__Camera
+        if self.__TestingItem_list == []:
+            Support = "No"
+        else :
+            Support = "Yes"
+
+        return self.__BSP_String ,self.__JetPack_String ,self.__Camera,Support
+
+    def get_TestingItem_list(self):
+        testingitem_name = []
+        testingeitem_bool =[]
+        for list_value in self.__TestingItem_list:
+            if list_value[1] != 0:
+                testingitem_name.append(list_value[0])
+
+        return testingitem_name
     
     def __get_BSP_Name(self):
         try:
             BSP_file = open("/proc/device-tree/nvidia,dtsfilename","r")
+            BSP_Text = BSP_file.read()
         except:
-            BSP_file =["R32_4_3_Xavier-NX_AN810_1.dts"]
-        BSP_Text = BSP_file.read()
+            BSP_Text ="R32_4_3_Xavier-NX_AN810_1.dts"
+
         BSP_list = BSP_Text.split("_")
         self.__BSP_list = BSP_list
         self.__BSP_String = BSP_list[0] + "_" + BSP_list[1] + "_" + BSP_list[2]
         self.__JetPack_String = "4." + BSP_list[1]
-        print(BSP_list[3],"\n")
+        #print(BSP_list[3],"\n")
         if len(BSP_list) >= 7:
             self.__Camera = BSP_list[5]
         else: 
             self.__Camera = "NO Camera"
-        self.__get_TestingItem()
+            
+        
+        #print(self.__TestingItem_list)
+
+
     def __get_TestingItem(self):
+        Testing_x = None
+        TestingItem_list = []
         dir_data = './data/'
         f_app = os.path.join(dir_data, 'TestingItem_2.csv')
         #print('Path of read in data: %s' % (f_app))
         TestingItem = pd.read_csv(f_app)
-        print(TestingItem.shape[0])
+        TestingItem_Col = TestingItem.columns.values.tolist()
+
         for i in range(TestingItem.shape[0]) :
             if TestingItem['Module'][i] == self.__BSP_list[3]:
                 if TestingItem['Board'][i] == self.__BSP_list[4]:
                     #print("this BSP is ",TestingItem['Module'][i],"_",TestingItem['Board'][i])
-                    print(TestingItem.ix[i])
+                    Testing_x = i
                     break
-                
+        
+        if Testing_x is not None:
+            for t_col in TestingItem_Col:
+                #print(TestingItem[t_col][Testing_x])
+                TestingItem_list.append([t_col,TestingItem[t_col][Testing_x]])
+        else:
+            TestingItem_list = []
+
+        #print(TestingItem_list)
+        return  TestingItem_list
+            
                 
